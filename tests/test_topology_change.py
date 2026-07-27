5b44b2d854301f4c86cb8bbdc4a5f78f3b0c0082
drosenbe <drosenbe@VDI0183.zit.bam.de>
2026-07-27 16:08:47 +0200

    remove old r_pf_probe tests

--- tests/test_topology_change.py
+++ tests/test_topology_change.py
@@ -2,7 +2,6 @@ import numpy as np
 import jax.numpy as jnp
 import pytest
 
-from reactive_md.reaction import make_probe_geometry
 from reactive_md.reaction import prepare_probe_geometry
 
 
@@ -14,48 +13,32 @@ def _shift(r, dr):
     return r + dr
 
 
-def test_make_probe_geometry_moves_leaving_f_along_pf_direction():
+def test_prepare_probe_geometry_does_not_change_unrelated_atoms():
     R = jnp.array(
         [
             [0.0, 0.0, 0.0],  # P
             [1.0, 0.0, 0.0],  # leaving F
+            [0.0, 5.0, 0.0],  # Li
+            [2.0, 2.0, 2.0],  # unrelated atom
         ],
         dtype=jnp.float32,
     )
 
-    R_new = make_probe_geometry(
+    R_new = prepare_probe_geometry(
         R,
         P_atom=0,
         leave_F=1,
+        li_idx=2,
+        sigma_p=2.0,
+        sigma_f=2.0,
         disp_fn=_disp,
         shift_fn=_shift,
-        r_pf_probe=4.0,
     )
 
-    np.testing.assert_allclose(np.asarray(R_new[0]), [0.0, 0.0, 0.0])
-    np.testing.assert_allclose(np.asarray(R_new[1]), [4.0, 0.0, 0.0])
-
-
-def test_make_probe_geometry_does_not_change_unrelated_atoms():
-    R = jnp.array(
-        [
-            [0.0, 0.0, 0.0],  # P
-            [1.0, 0.0, 0.0],  # leaving F
-            [0.0, 2.0, 0.0],  # unrelated atom
-        ],
-        dtype=jnp.float32,
+    np.testing.assert_allclose(
+        np.asarray(R_new[3]),
+        np.asarray(R[3]),
     )
-
-    R_new = make_probe_geometry(
-        R,
-        P_atom=0,
-        leave_F=1,
-        disp_fn=_disp,
-        shift_fn=_shift,
-        r_pf_probe=4.0,
-    )
-
-    np.testing.assert_allclose(np.asarray(R_new[2]), [0.0, 2.0, 0.0])
 
 
 def _free_space_functions():
